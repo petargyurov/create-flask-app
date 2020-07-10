c4073f226f46a84d30c625cd6e06cf31e9a6479b
Petar Gyurov <pgyurov93@gmail.com>
2020-07-10 15:11:16 +0100

    Formatting errors as JSON responses. Non-HTTP errors are treated as InternalServerError. Also fixed some formatting.

--- backend/__init__.py
+++ backend/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_cors import CORS
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.exceptions import HTTPException, InternalServerError
 
 cors = CORS()
 db = SQLAlchemy()
@@ -9,17 +10,29 @@ migrate = Migrate()
 
 
 def create_app():
-    app = Flask(__name__)
-    app.config.from_pyfile('backend.cfg')
-    cors.init_app(app, supports_credentials=app.config['SUPPORT_CREDENTIALS'],
-                  origins=[app.config['ORIGIN']])
-    db.init_app(app)
-    migrate.init_app(app, db)
+	app = Flask(__name__)
+	app.config.from_pyfile('backend.cfg')
+	cors.init_app(app, supports_credentials=app.config['SUPPORT_CREDENTIALS'],
+				  origins=[app.config['ORIGIN']])
+	db.init_app(app)
+	migrate.init_app(app, db)
 
-    from backend.example.endpoints import example
-    app.register_blueprint(example)
+	from backend.example.endpoints import example
+	app.register_blueprint(example)
 
-    return app
+	# format errors as JSON responses
+	@app.errorhandler(Exception)
+	def handle_exception(e):
+		if not isinstance(e, HTTPException):
+			e = InternalServerError()
+
+		return {
+			"code"       : e.code,
+			"name"       : e.name,
+			"description": e.description,
+		}
+
+	return app
 
 
 from backend.models import *
