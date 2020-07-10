2d62004dbfdf0b2c37daf5f27f1586f3e87329ee
Petar Gyurov <pgyurov93@gmail.com>
2020-07-10 13:35:07 +0100

    Now using a .cfg file for app config instead of .env to reduce unnecessary boilerplate

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,13 +1,12 @@
 from flask import Flask
 from flask_cors import CORS
-from backend.config import Config
 
 cors = CORS()
 
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object(Config)
+    app.config.from_pyfile('backend.cfg')
     cors.init_app(app, supports_credentials=app.config['SUPPORT_CREDENTIALS'],
                   origins=[app.config['ORIGIN']])
 
