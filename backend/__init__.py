3025a0be4f078f4d4b3b7197d9fbbf36fb5a8aa7
Petar Gyurov <pgyurov93@gmail.com>
2020-07-10 14:09:09 +0100

    Added Flask-SQLAlchemy and Flask-Migrate, plus initial migration.

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask
 from flask_cors import CORS
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 
 cors = CORS()
+db = SQLAlchemy()
+migrate = Migrate()
 
 
 def create_app():
@@ -9,8 +13,13 @@ def create_app():
     app.config.from_pyfile('backend.cfg')
     cors.init_app(app, supports_credentials=app.config['SUPPORT_CREDENTIALS'],
                   origins=[app.config['ORIGIN']])
+    db.init_app(app)
+    migrate.init_app(app, db)
 
     from backend.example.endpoints import example
     app.register_blueprint(example)
 
     return app
+
+
+from backend.models import *
