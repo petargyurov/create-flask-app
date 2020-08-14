ffdb8a58dcf52e98800a42f28a59d485d733b254
Petar Gyurov <pgyurov93@gmail.com>
2020-08-14 17:29:44 +0100

    Fixed the current tests

--- tests/test_example.py
+++ tests/test_example.py
@@ -5,11 +5,11 @@ from flask import url_for
 
 def test_get_user_success(client):
 	test_id = 1
-	resp = client.get(f"{url_for('example.get_user')}?id={test_id}")
+	resp = client.get(f"{url_for('get_user')}?id={test_id}")
 	assert resp.json['id'] == test_id
 
 
 def test_get_user_failure(client):
 	missing_id = 100
-	resp = client.get(f"{url_for('example.get_user')}id={missing_id}")
-	assert resp.json['code'] == 404
+	resp = client.get(f"{url_for('get_user')}id={missing_id}")
+	assert resp.status_code == 404
